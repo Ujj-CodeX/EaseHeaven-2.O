ec4053421373468f5cd807f68893f9f429a41aa7
Ujjawal Rauniyar <ujjawalrauniyar2004@gmail.com>
2025-06-19 13:16:53 +0530

    Professional registration End point

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from logging import error
 from flask import Flask, render_template,request,redirect, url_for,session, g, flash
 import os
 import sqlite3
+from werkzeug.security import check_password_hash
 
 
 app = Flask(__name__, template_folder='../templates')
@@ -50,7 +51,7 @@ def Pr_dash():
 def Service():
     return render_template('Admin_dash1.html')
 
-@app.route('/Partner Management')
+@app.route('/Partner_Management')
 def Partner_mng():
     return render_template('Admin_dash2.html')
 
@@ -135,6 +136,30 @@ def login_pg():
             return redirect(url_for('home'))
     return render_template('User_login.html')
 
+
+
+#-----------> Admin login
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        id = request.form['username']
+        password = request.form['password']
+
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM admin WHERE username = ? ", (id, ))
+        user = cursor.fetchone()
+
+        if user :
+            print(user)
+            if check_password_hash(user[2],password):
+                session['id'] = id
+                return redirect(url_for('Service'))
+        else:
+            flash('Invalid user ID or password.')
+            return redirect(url_for('Admin_login'))
+    return render_template('Admin_login.html')
 
 
 if __name__ == '__main__':
